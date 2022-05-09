44ba509e6e545a2efa78309241a2b72a295b0bfd
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-05-09 18:21:41 -0400

    Add files via upload

--- OCDM/OCDM.m
+++ OCDM/OCDM.m
@@ -1,13 +1,46 @@
 
 clear; clc; close all;
-N=64; %Number of Subcarrier
-L=4; %Channel Length
-Block_Num=100; %Block Number
+N=4; %Number of Subcarrier
+L=2; %Channel Length
+Block_Num=2; %Block Number
 M=4; %Modulation QAM
-C=4; %Len Cyclic Prefix 
-P=68;
+C=2; %Len Cyclic Prefix 
+P=N+C;
 loop_Num=10;
 Equal=1;
+S=eye(N);
+T=[S(2*N-P+1:N,:);S];
+R=[zeros(N,P-N),eye(N)];
+
+% Construct Fresnel Matrix
+DFnT0=zeros(N);
+DFnT1=zeros(N);
+if mod(N,2)==0
+    for m=1:N
+        for n=1:N
+            DFnT0(m,n)=sqrt(1/N)*exp(-1i*pi/4)*exp(1i*pi*(m-n)^2/N);
+        end
+    end
+end
+if mod(N,2)==1
+    for m=1:N
+        for n=1:N
+            DFnT1(m,n)=sqrt(1/N)*exp(-1i*pi/4)*exp(1i*pi*(m-n+0.5)^2/N);
+        end
+    end
+end        
+IDFnT0=DFnT0';
+IDFnT1=DFnT1';
+
+IFFT=zeros(N);
+for a=1:N
+    for b=1:N
+        IFFT(a,b)=exp(1i*2*pi*(a-1)*(b-1)/N);
+    end 
+end
+IFFT=IFFT*1/sqrt(N);
+FFT=conj(IFFT);
+%%
 total=zeros(1,6);  %preallocating for Speed, SNR from 0 to 20
 ratio=zeros(1,6);
 for dB=0:4:20
@@ -17,7 +50,7 @@ for dB=0:4:20
         [Bits,Trans_Symbols]=Transmitter(M,Block_Num,N,C);
         [H0,Symbols0]=Channel(Trans_Symbols,L,N,Block_Num,SNR);
         Bitsre=Receiver(M,Block_Num,N,C,Equal,Symbols0,H0,SNR);             
-        ratio(dB/4+1)=sum(Bits~=Bitsre)/(Block_Num*N);
+        ratio(dB/4+1)=sum(Bits~=Bitsre)/(Block_Num*N*log2(M));
         total(dB/4+1)=total(dB/4+1)+ratio(dB/4+1);
     end
 end
--- OCDM/Receiver.m
+++ OCDM/Receiver.m
@@ -26,47 +26,53 @@ end
 Y=zeros(N);
 if mod(N,2)==0
     for k=1:N
-        Y(k,k)=exp(-1i*pi*k^2/N);
+        Y(N-k+1,N-k+1)=exp(-1i*pi*k^2/N);
     end
 end
 if mod(N,2)==1
     for k=1:N
-        Y(k,k)=exp(-1i*pi*k*(k-1)/N);
+        Y(N-k+1,N-k+1)=exp(-1i*pi*k*(k-1)/N);
     end
 end
 %% Construct Equalization matrix 
 S=eye(N);
 T=[S(2*N-P+1:N,:);S];
 H=R*H0*T;
-V=FFT*H*FFT'; %%Diagonal Matrix 
+D=FFT*H*IFFT; %%Diagonal Matrix 
 if Equal==1
-    G=inv(V);
+    G=inv(D);
 end
 if Equal==2
-    G=conj(V)/(V^2+1/SNR);
+    G=conj(D)/(D^2+1/SNR);
 end
 %% Equalization
 Symbols4=zeros(size(Symbols3));
 for count=1:Block_Num
     Symbols4(:,:,count)=G*Y*Symbols3(:,:,count);
 end
+%% Apply IDFT 
+Symbols5=zeros(size(Symbols4));
+for count=1:Block_Num
+    Symbols5(:,:,count)=IFFT*Symbols4(:,:,count);
+end
 %% Demodulation
 if M==4
-    Symbols5=qamdemod(Symbols4/sqrt(1/2),M);
+    Symbols6=qamdemod(Symbols5/sqrt(1/2),M);
 end
 if M==16
-    Symbols5=qamdemod(Symbols4/sqrt(1/10),M);
+    Symbols6=qamdemod(Symbols5/sqrt(1/10),M);
 end
 if M==64
-    Symbols5=qamdemod(Symbols4/sqrt(1/42),M);
+    Symbols6=qamdemod(Symbols5/sqrt(1/42),M);
 end
 Bitsre=zeros(1,N*Block_Num*log2(M));
 start=1;
 for count=1:Block_Num
     for k=1:N
-        dec=dec2bin(Symbols5(k,1,count));
+        dec=dec2bin(Symbols6(k,1,count),log2(M));
         for n=1:length(dec)
             Bitsre(start)=str2double(dec(n));
+            start=start+1;
         end
     end
 end
