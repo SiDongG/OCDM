19b0ff58c7aa139b55b55abfc9e44894191848bd
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-07-03 15:32:11 -0400

    Add files via upload

---
+++ AdaptiveModulation_OCDM/VPVR_OCDM.m
@@ -0,0 +1,285 @@
+%% Variable Power Variable Rate Adaptive Modulation OCDM
+% Variable Power Variable Rate Block-wise Bit Loading OCDM using
+% Multi-carrier Spreading 
+
+clear; clc; close all;
+%% Parameter Initialization
+N=16; %Number of Subcarrier, assume always even 
+L=4; %Channel Length
+Block_Num=1; %Block Number
+C=4; %Len Cyclic Prefix 
+SNR=10;
+P=N+C;
+Q=4; %Size of subset \
+V=10; %Channel Variance 
+S=eye(N);
+T=[S(2*N-P+1:N,:);S];
+R=[zeros(N,P-N),eye(N)];
+M=2; %2-QAM (BPSK)
+Pb=1e-4;
+N_var=1;
+K=-1.5/(log(5*Pb)); % Above Cutoff Fade Level Constraint
+Power=N_var*SNR;
+Power_avg=Power/N;
+%% Matrix Initialization 
+IFFT=zeros(N);
+for a=1:N
+    for b=1:N
+        IFFT(a,b)=exp(1i*2*pi*(a-1)*(b-1)/N);
+    end 
+end
+IFFT=IFFT*1/sqrt(N);
+FFT=conj(IFFT);
+
+DFnT0=zeros(Q);
+if mod(Q,2)==0
+    for m=1:Q
+        for n=1:Q
+            DFnT0(m,n)=sqrt(1/Q)*exp(-1i*pi/4)*exp(1i*pi*(m-n)^2/Q);
+        end
+    end
+end
+IDFnT0=DFnT0';
+%% Initialize Channel Properties 
+h=V*(1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
+H0=zeros(P); %Preallocating for speed, H0 is the P by P matrix have the (i,j)th entry h(i-j)
+H1=zeros(P); %Preallocating for speed, H1 is the P by P matrix have the (i,j)th entry h(P+i-j)
+a=1;
+while a<P+1  %generate the channel matrces
+    b=1;
+    while b<P+1
+        if a-b<0 || a-b>L-1
+            H0(a,b)=0;
+        else
+            H0(a,b)=h(a-b+1);
+        end
+        if P+a-b<0 || P+a-b>L-1
+            H1(a,b)=0;
+        else
+            H1(a,b)=h(P+a-b+1);
+        end
+        b=b+1;
+    end
+    a=a+1;
+end
+D=FFT*R*H0*T*IFFT;
+%% Block-Wise Classification
+D_img=sort(diag(abs(D)));
+D_avg=zeros(1,length(D_img)/Q);
+for k=1:N/Q
+    D_avg(k)=mean(D_img(4*k-3:4*k));
+end
+%% Sub-Carrier Classification 
+y0=0.2; %Initialize Threshold and iteratively search for power balance
+yk=y0/K;
+Power_alloc=zeros(size(D_avg));
+QAM_alloc=zeros(size(D_avg));
+while abs(sum(Power_alloc)-Power)>Power/10   %Within 5% Accuracy
+    for count=1:N/Q
+        if (0<=D_avg(count)/yk) && (D_avg(count)/yk<2)
+            Power_alloc(count)=0;
+            QAM_alloc(count)=0;
+        elseif (2<=D_avg(count)/yk) && (D_avg(count)/yk<4)
+            Power_alloc(count)=Q*Power_avg*(1/(D_avg(count)*K));
+            QAM_alloc(count)=2;
+        elseif (4<=D_avg(count)/yk) && (D_avg(count)/yk<16)
+            Power_alloc(count)=Q*Power_avg*(3/(D_avg(count)*K));
+            QAM_alloc(count)=4;
+        elseif (16<=D_avg(count)/yk) && (D_avg(count)/yk<64)
+            Power_alloc(count)=Q*Power_avg*(15/(D_avg(count)*K));
+            QAM_alloc(count)=16;
+        else
+            Power_alloc(count)=Q*Power_avg*(63/(D_avg(count)*K));
+            QAM_alloc(count)=64;
+        end
+    end
+    if sum(Power_alloc)>Power+Power/10
+        yk=yk+0.00001;
+    elseif sum(Power_alloc)<Power-Power/10
+        yk=yk-0.00001;
+    else
+        yk=yk;
+    end
+    sum(Power_alloc)
+end
+%% Expanding Power and QAM Allocation 
+Power_alloc2=zeros(size(D_img));
+QAM_alloc2=zeros(size(D_img));
+for k=1:N/Q
+    Power_alloc2(4*k-3:4*k)=Power_alloc(k);
+    QAM_alloc2(4*k-3:4*k)=QAM_alloc(k);
+end
+% bar(D_img)
+% hold on;
+% plot([0,64],[2*yk,2*yk])
+% plot([0,64],[4*yk,4*yk])
+% plot([0,64],[16*yk,16*yk])
+% figure
+% bar(Power_alloc)
+sum(Power_alloc)
+%% Bit Generation 
+Total_Bits=0;
+for i=1:N
+    if QAM_alloc2(i)~=0
+        Total_Bits=Total_Bits+log2(QAM_alloc2(i));
+    end
+end
+%% Adaptive Modulation
+Nonz=nnz(QAM_alloc2);
+Symbols=zeros(N,1,Block_Num);
+Bits=randi(0:1,[1,Total_Bits*Block_Num]);
+Index=1;
+for count=1:Block_Num
+    for i=1:N
+        if QAM_alloc2(i)==0
+            Symbols(i,:,count)=0;
+        elseif QAM_alloc2(i)==2
+            B=bin2dec(num2str(Bits(Index)));
+            Symbols(i,:,count)=qammod(B,2);
+            Index=Index+1;
+        elseif QAM_alloc2(i)==4
+            B=bin2dec(num2str(Bits(Index:Index+1)));
+            Symbols(i,:,count)=qammod(B,4)*sqrt(0.5);
+            Index=Index+2;
+        elseif QAM_alloc2(i)==16
+            B=bin2dec(num2str(Bits(Index:Index+3)));
+            Symbols(i,:,count)=qammod(B,16)*sqrt(1/10);
+            Index=Index+4;
+        else
+            B=bin2dec(num2str(Bits(Index:Index+7)));
+            Symbols(i,:,count)=qammod(B,64)*sqrt(1/42);
+            Index=Index+8;
+        end
+    end
+end
+%% Power Adaptation 
+Symbols=Power_alloc2.*Symbols;
+%% Multi-Carrier Spreading
+Symbols2=zeros(size(Symbols));
+for count=1:Block_Num
+    Index=1;
+    for a=1:N/Q
+        Tu=zeros(Q,N);
+        for b=1:Q
+            for c=1:N
+                if c==Index
+                    Tu(b,c)=1;
+                else
+                    Tu(b,c)=0;
+                end
+            end
+            Index=Index+1;
+        end
+        Symbols2(:,:,count)=Symbols2(:,:,count)+Tu.'*IDFnT0*Symbols(Index-Q:Index-1,:,count);
+    end
+end
+%% Channel
+nr=randn(N,1,Block_Num);
+ni=randn(N,1,Block_Num);
+Noise=(sqrt(2)/2)*(nr+1i*ni);
+Symbols3=zeros(size(Symbols2));
+for count=1:Block_Num
+    Symbols3(:,:,count)=D*Symbols2(:,:,count)+Noise(:,:,count);
+end
+%% Despreading 
+Symbols4=zeros(size(Symbols3));
+G=pinv(D); %Composite Equalization Matrix
+for count=1:Block_Num
+    Index=1;
+    for a=1:N/Q
+        Tu=zeros(Q,N);
+        for b=1:Q
+            for c=1:N
+                if c==Index
+                    Tu(b,c)=1;
+                else
+                    Tu(b,c)=0;
+                end
+            end
+            Index=Index+1;
+        end
+        Symbols4(Index-Q:Index-1,:,count)=DFnT0*G(Index-Q:Index-1,Index-Q:Index-1)*Tu*Symbols3(:,:,count);
+    end
+end
+%% Demodulation
+Bitsre=zeros(size(Bits));
+Index=1;
+Symbols4=Symbols4./Power_alloc2;
+for count=1:Block_Num
+    for i=1:N
+        if QAM_alloc2(i)==2
+            A=qamdemod(Symbols4(i,:,count),2);
+            dec=dec2bin(A,1);
+            Bitsre(Index)=str2double(dec);
+            Index=Index+1;
+        elseif QAM_alloc2(i)==4
+            A=qamdemod(Symbols4(i,:,count),4);
+            dec=dec2bin(A,2);
+            Bitsre(Index)=str2double(dec(1));
+            Bitsre(Index+1)=str2double(dec(2));
+            Index=Index+2;
+        elseif QAM_alloc2(i)==16
+            A=qamdemod(Symbols4(i,:,count),16);
+            dec=dec2bin(A,4);
+            for n=1:length(dec)
+                Bitsre(Index+n)=str2double(dec(n));
+            end
+            Index=Index+4;
+        elseif QAM_alloc2(i)==64
+            A=qamdemod(Symbols4(i,:,count),64);
+            dec=dec2bin(A,8);
+            for n=1:length(dec)
+                Bitsre(Index+n)=str2double(dec(n));
+            end
+            Index=Index+8;
+        else
+            disp('')
+        end
+    end
+end
+%% Error Counting 
+Error=sum(Bitsre~=Bits);
+Error_rate=Error/length(Bits);
+%% Capacity Calculation 
+C_Nonadaptation=log2(2);
+C_Adaptation=0;
+for i=1:N
+    if QAM_alloc2(i)==2
+        C_Adaptation=C_Adaptation+log2(2)/N;
+    elseif QAM_alloc2(i)==4
+        C_Adaptation=C_Adaptation+log2(4)/N;
+    elseif QAM_alloc2(i)==16
+        C_Adaptation=C_Adaptation+log2(16)/N;
+    elseif QAM_alloc2(i)==64
+        C_Adaptation=C_Adaptation+log2(64)/N;
+    else
+        disp('')
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
