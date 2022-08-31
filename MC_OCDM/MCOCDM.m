7a005e803e8832ae2d5ba30ee074f6dca2d7ed9e
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-08-31 11:39:44 -0400

    Add files via upload

--- MC_OCDM/MCOCDM.m
+++ MC_OCDM/MCOCDM.m
@@ -1,16 +1,17 @@
 %% Multi-Carrier OCDM 
 %% Parameter Initialization
-function [Error_rate]=MCOCDM(K,N,L,Block_Num,C,SNR)
+function [Error_rate]=MCOCDM(SNR,Mode,M)
 % Use 64 sub-carriers, we spread the data symbols over a smaller set of
 % subcarriers, the size of each subset has to be divisible by total
 % sub-carrier length. 
-% K=8; %Size of subset 
-% N=16; %Number of Subcarrier, assume always even 
-% L=4; %Channel Length
-% Block_Num=2; %Block Number
-% C=4; %Len Cyclic Prefix 
-% SNR=10;
-M=4; %4QAM Modulation
+K=4; %Size of subset 
+N=64; %Number of Subcarrier, assume always even 
+L=4; %Channel Length
+Block_Num=100; %Block Number
+C=4; %Len Cyclic Prefix 
+% SNR=100;
+% Mode=1;
+% M=4; %4QAM Modulation
 P=N+C;
 S=eye(N);
 T=[S(2*N-P+1:N,:);S];
@@ -26,11 +27,20 @@ FFT=conj(IFFT);
 %% Bits Generation and Symbol Generation
 Bits=randi(0:1,[1,N*Block_Num*log2(M)]);
 Bits2=zeros(1,length(Bits)/log2(M));
-for i=1:log2(M):length(Bits)
-    Bits2(1+(i-1)/log2(M))=bin2dec(num2str(Bits(i:i+log2(M)-1)));
+for k=1:log2(M):length(Bits)
+    Bits2(1+(k-1)/log2(M))=bin2dec(num2str(Bits(k:k+log2(M)-1)));
+end
+if M==2
+    Bits3=qammod(Bits2,M);
 end
 if M==4
     Bits3=qammod(Bits2,M)*sqrt(0.5);
+end
+if M==16
+    Bits3=qammod(Bits2,M)*sqrt(1/10);
+end
+if M==64
+    Bits3=qammod(Bits2,M)*sqrt(1/42);
 end
 Symbols=reshape(Bits3,N,1,Block_Num);
 %% Multi-Carrier Spreading 
@@ -58,11 +68,11 @@ for count=1:Block_Num
             end
             Index=Index+1;
         end
-        Symbols2(:,:,count)=Symbols2(:,:,count)+IFFT*Tu.'*IDFnT0*Symbols(Index-K:Index-1,:,count);
+        Symbols2(:,:,count)=Symbols2(:,:,count)+Tu.'*IDFnT0*Symbols(Index-K:Index-1,:,count);
     end
 end
 %% Channel
-h=10*(1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
+h=(1/sqrt(2*L))*(randn(1,L)+1i*randn(1,L));
 H0=zeros(P); %Preallocating for speed, H0 is the P by P matrix have the (i,j)th entry h(i-j)
 H1=zeros(P); %Preallocating for speed, H1 is the P by P matrix have the (i,j)th entry h(P+i-j)
 a=1;
@@ -84,13 +94,21 @@ while a<P+1  %generate the channel matrces
     a=a+1;
 end
 H=R*H0*T;
+D=FFT*H*IFFT;
 %% AWGN
+D2=zeros(1,N);
+if Mode==2 %Interleave
+    for k=1:N/4
+        D2(4*k-3:4*k)=[D(k,k),D(k+16,k+16),D(k+32,k+32),D(k+48,k+48)];
+    end
+    D=diag(D2);
+end
 nr=randn(N,1,Block_Num);
 ni=randn(N,1,Block_Num);
 Noise=(sqrt(2)/2)*(nr+1i*ni);
 Symbols3=zeros(size(Symbols2));
 for count=1:Block_Num
-    Symbols3(:,:,count)=H*Symbols2(:,:,count)+(1/sqrt(SNR))*Noise(:,:,count);
+    Symbols3(:,:,count)=D*Symbols2(:,:,count)+(1/sqrt(SNR))*Noise(:,:,count);
 end
 %% Despreading 
 Symbols4=zeros(size(Symbols3));
@@ -109,12 +127,23 @@ for count=1:Block_Num
             end
             Index=Index+1;
         end
-        Symbols4(Index-K:Index-1,:,count)=DFnT0*G(Index-K:Index-1,Index-K:Index-1)*Tu*FFT*Symbols3(:,:,count);
+%         Symbols4(Index-K:Index-1,:,count)=DFnT0*G(Index-K:Index-1,Index-K:Index-1)*Tu*FFT*Symbols3(:,:,count);
+        Symbols3_5=DFnT0*Tu*Symbols3(:,:,count);
+        Symbols4(Index-K:Index-1,:,count)=ML_detector(Symbols3_5,M,DFnT0*Tu*D*Tu.'*IDFnT0);
     end
 end
 %% Demodulation
+if M==2
+    Symbols5=qamdemod(Symbols4,M);
+end
 if M==4
     Symbols5=qamdemod(Symbols4/sqrt(1/2),M);
+end
+if M==16
+    Symbols5=qamdemod(Symbols4/sqrt(1/10),M);
+end
+if M==64
+    Symbols5=qamdemod(Symbols4/sqrt(1/42),M);
 end
 Bitsre=zeros(1,N*Block_Num*log2(M));
 start=1;
--- MC_OCDM/MCOCDM_kernel.m
+++ MC_OCDM/MCOCDM_kernel.m
@@ -1,33 +1,43 @@
 clear; clc; close all;
-N=64; %Number of Subcarrier
-L=4; %Channel Length
-Block_Num=100; %Block Number
-C=4; %Len Cyclic Prefix 
-P=N+C;
-K=8;
-loop_Num=100;
+loop_Num=500;
 
-%% 
-total=zeros(1,6);  %preallocating for Speed, SNR from 0 to 20
-ratio=zeros(1,6);
-step=2;
+step=4;
 Start=0;
 End=20;
-for dB=Start:step:End
-    disp(dB);
-    SNR=10^(dB/10);
-    for loop=1:loop_Num
-        [Error_rate]=MCOCDM(K,N,L,Block_Num,C,SNR);
-        ratio(1,dB/step-Start/2+1)=Error_rate;
-        total(1,dB/step-Start/2+1)=total(1,dB/step-Start/2+1)+ratio(1,dB/step-Start/2+1);
+total=zeros(1,length(Start:step:End),3);  %preallocating for Speed, SNR from 0 to 20
+ratio=zeros(1,length(Start:step:End),3);
+total2=zeros(1,length(Start:step:End),3);  %preallocating for Speed, SNR from 0 to 20
+ratio2=zeros(1,length(Start:step:End),3);
+Mod=[2,4,16];
+
+for SNRdB=Start:step:End
+    disp(SNRdB);
+    SNR=10^(SNRdB/10);
+    for U=1:2
+        M=Mod(U);
+        for loop=1:loop_Num
+            [Error]=MCOCDM(SNR,1,M);            
+            ratio(1,SNRdB/step-Start/2+1,U)=Error;
+            total(1,SNRdB/step-Start/2+1,U)=total(1,SNRdB/step-Start/2+1,U)+ratio(1,SNRdB/step-Start/2+1,U);
+            [Error_int]=MCOCDM(SNR,2,M);       
+            ratio2(1,SNRdB/step-Start/2+1,U)=Error_int;
+            total2(1,SNRdB/step-Start/2+1,U)=total2(1,SNRdB/step-Start/2+1,U)+ratio2(1,SNRdB/step-Start/2+1,U);
+        end
     end
 end
+
 total=total/loop_Num;
+total2=total2/loop_Num;
 figure()
 box on; hold on;
-plot(Start:step:End,total,'bx-');
+plot(Start:step:End,total(:,:,1),'bx-');
+plot(Start:step:End,total(:,:,2),'rx-');
+% plot(Start:step:End,total(:,:,3));
+plot(Start:step:End,total2(:,:,1),'gx-');
+plot(Start:step:End,total2(:,:,2));
+% plot(Start:step:End,total2(:,:,3));
 set(gca,'Yscale','log');
-ylim([1e-4 1]);
+ylim([1e-5 1]);
 xlabel('SNR(dB)');
 ylabel('Ber');
-legend('OCDM')
+legend('OCDM 2QAM','OCDM 4QAM','OCDM 2QAM In','OCDM 4QAM In')
